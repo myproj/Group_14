a44a9f0556c2c050083b9853e7a995d849b2a274
akv <ashutoshkrverma@outlook.com>
2017-09-04 11:00:45 -0700

    modified branch

--- factorial.cpp
+++ factorial.cpp
@@ -1,10 +1,14 @@
 #include "functions.h"
+#include <stdio.h>
 
 int factorial(int n){
-	if(n<0){
+	if(n<0)
+	  {
+	    printf("%s","Factorial of negative numbers do not exist");
 		return -1;
 	}
-    if(n!=1){
+    if(n!=1)
+      {
 	return(n * factorial(n-1));
     }
     else return 1;
