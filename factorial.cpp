2f5f87446b9a70abf87712bd681e1cf74e7ccac1
akv <ashutoshkrverma@outlook.com>
2017-09-04 11:25:43 -0700

    factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,11 +1,6 @@
 #include "functions.h"
 #include <stdio.h>
 
-<<<<<<< HEAD
-int factorial(int n)
-{
-    if(n!=1){
-=======
 int factorial(int n){
 	if(n<0)
 	  {
@@ -14,7 +9,6 @@ int factorial(int n){
 	}
     if(n!=1)
       {
->>>>>>> branch1
 	return(n * factorial(n-1));
     }
     else return 1;
